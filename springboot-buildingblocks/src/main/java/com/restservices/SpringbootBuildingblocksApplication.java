cc2bb44c8627ca9586fb17215290634c3d81c475
atharvaduse <atharvaduse@gmail.com>
2024-08-22 22:28:09 +0530

    second commt

--- springboot-buildingblocks/src/main/java/com/restservices/SpringbootBuildingblocksApplication.java
+++ springboot-buildingblocks/src/main/java/com/restservices/SpringbootBuildingblocksApplication.java
@@ -2,7 +2,9 @@ package com.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
+
+//token -  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
+
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
