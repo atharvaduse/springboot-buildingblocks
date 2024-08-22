963dfcb080e3064cabf94c4367c8a0816ce0edc1
atharvaduse <atharvaduse@gmail.com>
2024-08-22 22:29:46 +0530

    commit

--- springboot-buildingblocks/src/main/java/com/restservices/SpringbootBuildingblocksApplication.java
+++ springboot-buildingblocks/src/main/java/com/restservices/SpringbootBuildingblocksApplication.java
@@ -3,7 +3,7 @@ package com.restservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//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  - token
+//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  -token
 
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
